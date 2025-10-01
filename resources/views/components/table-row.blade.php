b497370a14a8586f55d547be090e7c2053e761c7
Robroi Neo M. Dingal <robroineo2@gmail.com>
2025-10-02 03:44:29 +0800

    refactor table-row, dynamic value passing

--- resources/views/components/table-row.blade.php
+++ resources/views/components/table-row.blade.php
@@ -1,8 +1,11 @@
-<tr class="hover:bg-gray-50  border-neutral-200 border-b-1">
+@props([
+    'values' => ['1', 'ABC Supplies', 'Main Street'],
+])
 
+<tr class="hover:bg-gray-50  border-neutral-200 border-b-1">
     <!-- populate with database later -->
-    <x-table-data type="data" value="1"></x-table-data>
-    <x-table-data type="data" value="ABC Supplies"></x-table-data>
-    <x-table-data type="data" value="123 Main St, City"></x-table-data>
+    @foreach ($values as $value)
+        <x-table-data type="data" value="{{ $value }}"></x-table-data>
+    @endforeach
     <x-table-data type="action_btns"></x-table-data>
 </tr>
