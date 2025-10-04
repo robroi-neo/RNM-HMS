ca58a05971a4bbb89be5a532bb2b5a28b62edc25
Robroi Neo M. Dingal <robroineo2@gmail.com>
2025-10-05 02:07:47 +0800

    add no available data view in tale

--- resources/views/components/tables.blade.php
+++ resources/views/components/tables.blade.php
@@ -3,18 +3,25 @@
 ])
 
 <div class="overflow-x-auto rounded-lg shadow">
-    <table class="min-w-full text-sm text-left">
-        <thead class="bg-gray-200 text-gray-700 text-lg">
+    <table class="min-w-full text-lg text-left">
+        <thead class="bg-gray-200 text-gray-700">
             <tr>
                 <th class="px-4 py-4 w-40">Supplier ID</th>
                 <th class="px-4 py-4">Company Name</th>
                 <th class="px-4 py-4">Contact Person</th>
                 <th class="px-4 py-4">Contact Number</th>
-                <th class="px-4 py-4 text-center"></th>
+                <th class="px-4 py-4 text-center">Action Button</th>
             </tr>
         </thead>
 
         <tbody>
+            @if (count($suppliers) === 0)
+                <tr>
+                    <td colspan="5" class="px-4 py-4 text-center text-gray-600 bg-gray-100 ">
+                        No data available in table
+                    </td>
+                </tr>
+            @endif
             @foreach ($suppliers as $supplier)
                 <x-table-row :values="[
                     $supplier['id'],
