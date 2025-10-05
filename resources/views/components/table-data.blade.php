d2deff1ebb3d2c1e4bee5dd398d830c2522c0741
Robroi Neo M. Dingal <robroineo2@gmail.com>
2025-10-06 02:06:16 +0800

    add stopPropogation to delete and edit button

--- resources/views/components/table-data.blade.php
+++ resources/views/components/table-data.blade.php
@@ -10,8 +10,9 @@
 @else
     <td class="flex flex-row px-10 py-4 text-center justify-end space-x-3">
         <!-- Delete icon -->
-        <a class="text-center">
-            <svg class="w-6 h-6 text-neutral-500 hover:text-neutral-900" viewBox="0 0 25 25" fill="none"
+        <a class="text-center px-1 py-0.5" onclick="event.stopPropagation();">
+            <svg class="w-6 h-6
+            text-neutral-500 hover:text-neutral-900" viewBox="0 0 25 25" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path d="M1.78564 6.25H23.2142" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                     stroke-linejoin="round" />
@@ -29,8 +30,9 @@
             </svg>
         </a>
         <!-- Edit Icon -->
-        <a class="text-center">
-            <svg class="w-6 h-6 text-neutral-500 hover:text-neutral-900" viewBox="0 0 25 25" fill="none"
+        <a class="text-center px-1 py-0.5" onclick="event.stopPropagation();">
+            <svg class="w-6 h-6 text-neutral-500
+            hover:text-neutral-900" viewBox="0 0 25 25" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M13.3933 16.0714L8.03613 17.0357L8.92899 11.6071L19.1611 1.41072C19.3271 1.24335 19.5246 1.1105 19.7422 1.01984C19.9599 0.929183 20.1933 0.882507 20.429 0.882507C20.6647 0.882507 20.8981 0.929183 21.1157 1.01984C21.3333 1.1105 21.5308 1.24335 21.6968 1.41072L23.5897 3.30358C23.7571 3.46958 23.8899 3.66708 23.9806 3.88469C24.0712 4.10229 24.1179 4.3357 24.1179 4.57143C24.1179 4.80717 24.0712 5.04057 23.9806 5.25818C23.8899 5.47578 23.7571 5.67328 23.5897 5.83929L13.3933 16.0714Z"
