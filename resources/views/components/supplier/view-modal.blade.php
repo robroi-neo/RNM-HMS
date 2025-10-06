4050856da9f0a1d0305e661edf4f7e0f5e3f4071
Robroi Neo M. Dingal <robroineo2@gmail.com>
2025-10-06 13:00:25 +0800

    redesign view-modal for supplier

--- resources/views/components/supplier/view-modal.blade.php
+++ resources/views/components/supplier/view-modal.blade.php
@@ -3,13 +3,31 @@
     <!-- Backdrop -->
     <div onclick="toggleSupplierDetails(false)" class="absolute inset-0 bg-gray-500/75"></div>
     <!-- Popup Box -->
-    <div class="relative bg-white p-6 rounded-2xl shadow-lg w-96 z-10 space-y-2">
+    <div class="relative bg-white p-6 rounded-2xl shadow-lg w-112 z-10 space-y-2">
         <h2 class="text-xl font-semibold mb-2">Supplier Details</h2>
-        <p><strong>id:</strong> <span id="modalId"></span></p>
-        <p><strong>Name:</strong> <span id="modalName"></span></p>
-        <p><strong>Address:</strong> <span id="modalAddress"></span></p>
-        <p><strong>Contact Person:</strong> <span id="modalPerson"></span></p>
-        <p><strong>Contact Number:</strong> <span id="modalNumber"></span></p>
+        <div class="space-y-2 text-lg">
+            <div class="grid grid-cols-2 gap-1">
+                <p class="font-semibold text-gray-700">Supplier ID:</p>
+                <p id="modalId" class="text-left text-gray-800"></p>
+            </div>
+            <div class="grid grid-cols-2 gap-1">
+                <p class="font-semibold text-gray-700">Supplier Name:</p>
+                <p id="modalName" class="text-left text-gray-800"></p>
+            </div>
+            <div class="grid grid-cols-2 gap-1">
+                <p class="font-semibold text-gray-700">Supplier Address:</p>
+                <p id="modalAddress" class="text-left text-gray-800"></p>
+            </div>
+            <div class="grid grid-cols-2 gap-1">
+                <p class="font-semibold text-gray-700">Contact Person:</p>
+                <p id="modalPerson" class="text-left text-gray-800"></p>
+            </div>
+            <div class="grid grid-cols-2 gap-1">
+                <p class="font-semibold text-gray-700">Contact Number:</p>
+                <p id="modalNumber" class="text-left text-gray-800"></p>
+            </div>
+        </div>
+
 
         <div class="flex justify-end mt-4">
             <button onclick="toggleSupplierDetails(false)"
